ba8aa46818228ec8368ff1eca23c5ac4966ec80f
shaun-mcfadden <87025314+shaun-mcfadden@users.noreply.github.com>
2022-11-07 16:39:44 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String x = "hello";
+        String y = "llehp";
+
+        char a[] = x.toCharArray();
+        char b[] = y.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        Boolean result = Arrays.equals(a,b);
+
+        if(result == true)
+        {
+            System.out.println("Strings are Anagram");
+        }
+
+        else
+        {
+            System.out.println("Strings are not Anagram");
+        }
+
+    }
+}
